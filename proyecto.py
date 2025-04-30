152b75905faf2b78569004707925a6da88d0a2aa
SebaOre <seb.orellanab@duocuc.cl>
2025-04-29 21:36:17 -0400

    edicion calculadora

--- proyecto.py
+++ proyecto.py
@@ -1,1 +1,21 @@
 print("el terrible proyecto")
+print("calculadora brijida")
+while True:
+    print("1.sumar")
+    print("2.restar")
+    print("3.salir")
+    opc=int(input("ingrese una opcion: "))
+    if opc==1:
+        num1=int(input("ingrese un numero:"))
+        num2=int(input("ingrese un numero:"))
+        print("el resuletado es: ",num1+num2)
+    elif opc==2:
+        num1=int(input("ingrese un numero:"))
+        num2=int(input("ingrese un numero:"))
+        print("el resuletado es: ",num1-num2)
+    elif opc==3:
+        break;
+    else:
+        print("ingrese un numero valido")
+        
+        
